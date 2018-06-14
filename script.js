df7ab104cebefd5e024f08c3ae56e665d2f63e3c
Joe Friel <joseph.s.friel@gmail.com>
2018-06-14 12:13:52 +0100

    Add decimal functionability

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let screen = document.getElementById("screen"); 
-let output;
+let output = "";
 let equal = false;
 
 // Insert numbers onto screen
@@ -7,7 +7,7 @@ let num = document.querySelectorAll(".num-button");
 
     for(let i=0; i < num.length; i++) {
         num[i].addEventListener("click", function() {
-             if(screen.innerHTML == 0 || output == 0) {
+             if(output.length == 0) {
                 screen.innerHTML = num[i].value;
                 output = num[i].value;
             } else if(equal == true && store == "") {
@@ -16,8 +16,14 @@ let num = document.querySelectorAll(".num-button");
                 equal = false;
             } 
             else {
-                output = screen.innerHTML += num[i].value;
-                screen.innerHTML = output;
+                if (screen.innerHTML == "0.") {
+                    output = output.concat(num[i].value);
+                    screen.innerHTML = output;
+                } else {
+                    output = screen.innerHTML.concat(num[i].value);
+                    screen.innerHTML = output;
+                }
+                
             } 
             // responsive font size 
             let count = screen.innerHTML;
@@ -33,6 +39,21 @@ let num = document.querySelectorAll(".num-button");
         })
     }
 
+// Add decimal
+let decimal = document.getElementById("decimal")
+let testRegex = /\./;
+
+decimal.addEventListener("click", function() {
+    if(output == "" || screen.innerHTML == 0 || equal == true) {
+        screen.innerHTML = "0.";
+        output = "0.";
+        equal = false;
+    } else if(testRegex.test(output) != true) {
+        output = screen.innerHTML.concat(".");
+        screen.innerHTML = output;
+    }
+})
+
 
 // All Clear operation
 let allClear = document.getElementById("all-clear"); 
@@ -40,7 +61,7 @@ let allClear = document.getElementById("all-clear");
 allClear.addEventListener("click", function() {
     document.getElementById("screen").style.fontSize = "4em";
     screen.innerHTML = 0;
-    output = 0;
+    output = "";
     store = "";
     equal = false;
 })
@@ -68,7 +89,7 @@ for(let i=0; i < op.length; i++) {
         // add/subtract/multiply/divide & stores value
         if(op[i].id == "add" || op[i].id == "subtract" || op[i].id == "multiply" || op[i].id == "divide") {
             store = store.concat(output, this.name);
-            output = 0;
+            output = "";
         }
         // equals function
             else if(op[i].id == "equals") {
